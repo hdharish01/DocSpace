b46fe9a9a7192d350d1f12ad5c5b6e0d5286cdc4
Harish kumar <hdharish0101@gmail.com>
2024-10-04 10:31:31 +0530

    added authentication

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,19 @@
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Document(){
     return <div>
         <Header>
-            <p className="text-white">Test</p>
+            <div className="flex w-fit items-center justify-center gap-2">
+                <div className="document-title">share</div>
+            </div>
+            <SignedOut>
+                <SignInButton></SignInButton>
+            </SignedOut>
+            <SignedIn>
+                <UserButton></UserButton>
+            </SignedIn>
         </Header>
         <Editor />
     </div>
